b732fbc6fb505afeeb9fdfc230b171057c122015
J0HN Z0MB1E <johnzombie1992@gmail.com>
2017-09-13 10:43:36 -0400

    added if statements and functions

--- gitscript.sh
+++ gitscript.sh
@@ -1,94 +1,81 @@
 #!/bin/bash
 
 
-OS_var=$(uname)
-distro_var=$(cat /etc/*-release | head -n1)
-
-
-
-
-
+#OS_var=$(uname)
+#distro_var=$(cat /etc/*-release | head -n1)
+YUM_CMD=$(which yum)
+APT_GET_CMD=$(which apt-get)
+ZYPPER_CMD=$(which zypper)
+PACMAN_CMD=$(which pacman)
+########################################
+#echo spacer
+#######################################
+function echo_spacer ()
+{
+echo
+echo
+echo
+echo
+echo
+}
 ##############################
 #ssh download check function
 ##############################
 function ssh_download ()
-
 {
-
-
-if [[ $? != 0 ]]; then 
-
-if [[ "$OS_var" == "Linux" ]]; then
-
-if [[ "$distro_var" =~ ..Ubuntu ]]; then
-cd ~
-sudo apt-get install openssh-server
-sudo service ssh start
-service ssh status | grep active
-read
-clear
-
-elif [[ "$distro_var" =~ CentOS.. ]]; then
+echo " Checking distro and installing ssh..."
+if [[ ! -z $YUM_CMD ]]; then
 sudo yum install openssh-server
 sudo service ssh start
-service ssh status | grep active
-
-elif [[ "$distro_var" =~ openSUSE ]]; then
-sudo zypper install openssh-server
+sudo service ssh status | grep active 
+elif [[ ! -z $APT_GET_CMD ]]; then
+sudo apt-get install openssh-server
 sudo service ssh start
 sudo service ssh status | grep active
-
-
-
+elif [[ ! -z $ZYPPER_CMD ]]; then
+sudo zypper refresh
+sudo zypper up
+sudo zypper install openssh-server
 else
-echo "Sorry you will need to install ssh manually"
-echo
-echo
-read
-echo "Installtion may vary depending on distro."
-exit
+echo "error can't install ssh...Please install manually"
+exit 1;
 fi
-
-
-
-
-
-
-
+}
 ##############################
 #SSH setup function for Github
 ##############################
-
 function git_ssh ()
 {
  
-	while [ -z "$select" ]
-	do
-		echo " We will now give you the option to install ssh if you do not already have it on your machine or generate your public key to connect with ssh and perform git clones via ssh for desired repos..
-
-
+while [ -z "$select" ]
+do
+echo " Now we will create a folder named .ssh in your home directory and generate a public ssh key for ssh cloning from github to your local machine. "
+read
+clear
 cd ~
 cd .ssh
-
-
-
-
-
-
-
-
-
+echo " Please hit enter multiple times as the key generating prompts are running. No need to enter a paraphrase it will generate one for you."
+echo_spacer
+ssh-keygen -t rsa
+clear
 ####################
 # Distro Functions
 ####################
 function debian_git ()
-
 {
-
-
-echo " Hit enter to install Github. This will install in the home directory."
+echo " Hit enter to install Github. This will install in the home directory and create a folder named github in the documents directory."
 read
 cd ~
+if [[ ! -z $YUM_CMD ]]; then
+sudo yum install git
+elif [[ ! -z $APT_GET_CMD ]]; then
+sudo apt-get install git
+elif [[ ! -z $OTHER_CMD ]]; then
+$OTHER_CMD <proper arguments>
+else
+echo "error can't install package $PACKAGE"
+exit 1;
+fi
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install git
 clear
@@ -96,41 +83,19 @@ echo " A folder named github will now be created in the Documents directory. It 
 read
 cd Documents 
 mkdir github
-
-
-
-
-
-
-
-
-
-
-
 #####################
 #Selection Menu
 #####################
-
-
-
-
 while [ -z "$choice" ]
 do
-
-
 echo " Please your distro that you wish to install Github on.. "
 
        echo " 1. ~Debian-based~ "
             " 2. ~RedHat-based~ "
             " 3. ~OpenSUSE~     "
             " 4. ~ArchLinux~    "
-
-
-
 read -r choice;
-
 done
-
 case $choice in
 	1) 
 
