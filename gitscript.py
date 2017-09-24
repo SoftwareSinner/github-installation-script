b73249f71076f8a6372f502d90a8157bc727c884
J0HN Z0MB1E <johnzombie1992@gmail.com>
2017-09-24 19:49:52 -0400

    added more variables

--- gitscript.py
+++ gitscript.py
@@ -3,10 +3,12 @@ import getpass
 from github import Github
 import os
 input("Welcome to the github setup script...Please press enter to proceed.")
-if os.uname == "fedora":
+yum = os.system('which yum')
+apt-get == os.system('which apt-get')
+if yum == "usr/bin/yum":
     os.system ('cd ~')
-    os.system('sudo dnf update')
-    os.system('dnf install git')
+    os.system('sudo yum install update')
+    os.system('sudo yum install git')
     os.system('cd Documents') 
     print("A folder named githubprojects will now be created in your documents directory")
     os.system('mkdir githubprojects')
