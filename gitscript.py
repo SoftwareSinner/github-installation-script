2d6d3caa8d64671a2412bef8e5727b971ae0533c
J0HN Z0MB1E <johnzombie1992@gmail.com>
2017-09-25 20:59:18 -0400

    added more functions

--- gitscript.py
+++ gitscript.py
@@ -2,17 +2,17 @@
 import getpass
 from github import Github
 import os
+import sys
 input("Welcome to the github setup script...Please press enter to proceed.")
-#apt-get == os.system('which apt-get')
 def yum_func():
     yum = os.system('which yum')
     if yum == 0:
     os.system('cd ~')
     os.system('sudo yum install update')
     os.system('sudo yum install git')
-    os.system('yum -y groupinstall development')
-    os.system('yum -y install zlib-devel')
-    os.system('wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz')
+    os.system('sudo yum -y groupinstall development')
+    os.system('sudo yum -y install zlib-devel')
+    os.system('sudo wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz')
     os.system('tar xJf Python-3.6.0.tar.xz')
     os.system('cd Python-3.6.0')
     os.system('./configure')
@@ -21,20 +21,34 @@ def yum_func():
     os.system('clear')
     print("Python is now on your system!")
     os.system('python3 -V')
+    print("SSH will now be installed.")
     os.system('sudo yum install openssh-server')
     os.system('sudo systemctl enable sshd.service')
     os.system('sudo systemctl status sshd.service | grep active')
+    os.system('sudo yum install jq')
     print("A folder named githubprojects will now be created in your documents directory")
     os.system('cd Documents')
     os.system('mkdir githubprojects')
-
 def deb_func():
-    apt-get == os.system('which apt-get')
-
+    apt-get = os.system('which apt-get')
+    if apt-get == 0:
+    os.system('sudo apt-get install python3.6')
+    os.system('sudo apt-get install python-pip')
+    os.system('sudo apt-get install openssh-server')
+    os.system('sudo service ssh start')
+    os.system('sudo service ssh status | grep active')
+    os.system('sudo apt-get update')
+    os.system('sudo apt-get install git') 
+    os.system('sudo apt-get install jq')
 def git_ssh():
     print("SSH key is now being created..")
     os.system('ssh-keygen -t rsa') 
-
+    os.system('KEY=$(sudo cat ~/.ssh/id_rsa.pub)')
+    os.system('echo "Here is your KEY var: ${KEY}"')
+    os.system('clear')
+    os.system('read -p "GitHub Username: " USERNAME')
+    os.system('read -p "Please enter a title for you ssh key: " TITLE')
+    os.system('jq -n --arg t "$TITLE" --arg k "$KEY" '{title: $t, key: $k}' | curl --user "$USERNAME" -X POST --data @- https://api.github.com/user/keys'
 def git_auth():
     githubUser = input("Please type in your username : ")
     githubPassword = getpass.getpass("Please type password for your github account: ")
